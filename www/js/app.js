023ebb577e0c8d8f8e58a291d835d8ff09f51b49
CRUX56 <designers_crux@live.com>
2017-01-26 00:15:03 -0500

    Implentations
    Finished implenting modal boxes,
    Implemented parallax on footer element
    Sass-lint updates

---
+++ www/js/app.js
@@ -0,0 +1,106 @@
+$(document).ready(function(){
+
+  //TEST console.log("Hello Dathan, Javascript is working");
+
+  //STICKY MENU
+
+  $(function(){
+    $(window).scroll(function(){
+      if ($(this).scrollTop() >= 290) {
+        $('header').addClass('stickytop');
+      }
+      else {
+        $('header').removeClass('stickytop');
+      }
+    });
+  });
+
+  //MOBILE MENU DROPDOWN
+
+  function mobileMenu() {
+    document.getElementById('mobileDropdown').classList.toggle('show');
+  }
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')){
+
+      var dropdowns = document.getElementsByClassName('dropdown-content');
+      var i;
+      for (i = 0;  i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  //MODAL BOX FOR SPEED YOU NEED SERVICE-BOX
+
+  // Get the modal for Speed
+  var speedmodal = document.getElementById('speedModal');
+
+  // Get the button that opens the modal
+  var speedbtn = document.getElementById("speedBtn");
+
+  // Get the <span> element that closes the modal
+  var exit = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  speedbtn.onclick = function() {
+    speedmodal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  exit.onclick = function() {
+    speedmodal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == speedmodal) {
+        speedmodal.style.display = "none";
+    }
+  }
+
+  //MODAL BOX FOR FEATURES YOU WANT SERVICE-BOX
+
+  var featuremodal = document.getElementById('featureModal');
+  var featurebtn = document.getElementById("featureBtn");
+  var exitspan = document.getElementsByClassName("close")[0];
+
+  featurebtn.onclick = function() {
+    featuremodal.style.display = "block";
+  }
+
+  exitspan.onclick = function() {
+    featuremodal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == featuremodal) {
+        featuremodal.style.display = "none";
+    }
+  }
+
+  // MODAL BOX FOR PRICE SERVICE SECTION
+
+  var pricemodal = document.getElementById('priceModal');
+  var pricebtn = document.getElementById('priceBtn');
+  var exitbtn = document.getElementsByClassName('close')[0];
+
+  pricebtn.onclick = function() {
+    pricemodal.style.display = "block";
+  }
+
+  exitbtn.onclick = function() {
+    pricemodal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == pricemodal) {
+      pricemodal.style.display = "none";
+    }
+  }
+
+});
