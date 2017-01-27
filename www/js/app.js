e6b30225531a75982beaab115d94f64938c10da1
CRUX56 <designers_crux@live.com>
2017-01-26 23:58:36 -0500

    Code Tidying
    Reworked elements in sass for more efficiency.
    Added mobile menu
    Worked on JavaScript elements

--- www/js/app.js
+++ www/js/app.js
@@ -15,26 +15,6 @@ $(document).ready(function(){
     });
   });
 
-  //MOBILE MENU DROPDOWN
-
-  function mobileMenu() {
-    document.getElementById('mobileDropdown').classList.toggle('show');
-  }
-
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')){
-
-      var dropdowns = document.getElementsByClassName('dropdown-content');
-      var i;
-      for (i = 0;  i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
-
   //MODAL BOX FOR SPEED YOU NEED SERVICE-BOX
 
   // Get the modal for Speed
